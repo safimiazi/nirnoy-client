07d4218b3e4c67ec761e49628642bfb2ca7fd109
safi miazi <mohibullamiazi@gmail.com>
2025-02-23 13:38:09 +0600

    feat: add authentication form component and integrate Ant Design for improved user experience

---
+++ app/auth/page.tsx
@@ -0,0 +1,152 @@
+"use client";
+import React, { useState } from "react";
+import { Form, Input, Button } from "antd";
+import {
+  UserOutlined,
+  LockOutlined,
+  MailOutlined,
+  PhoneOutlined,
+} from "@ant-design/icons";
+
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleForm = () => {
+    setIsLogin(!isLogin);
+  };
+
+  const onFinish = (values: any) => {
+    console.log("Form values:", values);
+  };
+
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-800">
+      <div className="bg-white dark:bg-gray-700 p-8 rounded-lg shadow-lg w-96 transform transition-all duration-500 ease-in-out hover:scale-105">
+   
+
+        <h1 className="text-2xl font-bold mb-6 text-center text-black dark:text-white">
+          {isLogin ? "Login" : "Register"}
+        </h1>
+
+        <Form name="auth-form" onFinish={onFinish}>
+          {!isLogin && (
+            <Form.Item
+              name="name"
+              rules={[{ required: true, message: "Please input your name!" }]}
+            >
+              <Input
+                prefix={
+                  <UserOutlined className="site-form-item-icon text-black dark:text-white" />
+                }
+                placeholder="Name"
+                className="text-black dark:text-white dark:placeholder-gray-300"
+              />
+            </Form.Item>
+          )}
+
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input
+              prefix={
+                <MailOutlined className="site-form-item-icon text-black dark:text-white" />
+              }
+              placeholder="Email"
+              className="text-black dark:text-white dark:placeholder-gray-300"
+            />
+          </Form.Item>
+
+
+          {!isLogin && (
+            <Form.Item
+              name="contactNo"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your contact number!",
+                },
+                {
+                  pattern: /^[0-9]{10}$/,
+                  message: "Please enter a valid 10-digit contact number!",
+                },
+              ]}
+            >
+              <Input
+                prefix={
+                  <PhoneOutlined className="site-form-item-icon text-black dark:text-white" />
+                }
+                placeholder="Contact Number"
+                className="text-black dark:text-white dark:placeholder-gray-300"
+              />
+            </Form.Item>
+          )}
+
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password
+              prefix={
+                <LockOutlined className="site-form-item-icon text-black dark:text-white" />
+              }
+              placeholder="Password"
+              className="text-black dark:text-white dark:placeholder-gray-300"
+            />
+          </Form.Item>
+
+          {/* {!isLogin && (
+            <Form.Item
+              name="confirmPassword"
+              rules={[
+                { required: true, message: "Please confirm your password!" },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error("The two passwords do not match!")
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Input.Password
+                prefix={
+                  <LockOutlined className="site-form-item-icon text-black dark:text-white" />
+                }
+                placeholder="Confirm Password"
+                className="text-black dark:text-white dark:placeholder-gray-300"
+              />
+            </Form.Item>
+          )} */}
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="w-full text-white"
+            >
+              {isLogin ? "Login" : "Register"}
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <div className="text-center mt-4">
+          <span className="text-gray-600 dark:text-gray-300">
+            {isLogin ? "Don't have an account?" : "Already have an account?"}
+          </span>
+          <Button type="link" onClick={toggleForm} className="dark:text-white text-black dark:hover:text-gray-50">
+            {isLogin ? "Register here" : "Login here"}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
--- components/common/ClientLayout.tsx
+++ components/common/ClientLayout.tsx
@@ -4,22 +4,34 @@ import { Provider } from "react-redux";
 import { store } from "../redux/Store";
 import { ThemeProvider } from "../theme-provider";
 import { ReduxProvider } from "../redux/ReduxProvider";
+import { ConfigProvider } from "antd";
 
-  // Mark this as a Client Component
+// Mark this as a Client Component
 
-
-
-export default function ClientLayout({ children }: { children: React.ReactNode }) {
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <Provider store={store}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#000000",
+            colorInfo: "#000000",
+          },
+        }}
       >
-        <ReduxProvider>{children}</ReduxProvider>
-      </ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ReduxProvider>{children}</ReduxProvider>
+        </ThemeProvider>
+      </ConfigProvider>
     </Provider>
   );
 }
